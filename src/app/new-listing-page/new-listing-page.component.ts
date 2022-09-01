7b0cb81f7833186147382afba936c579bb677d39
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-09-01 16:40:56 +0200

    Create new-listings-page component markup

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
   templateUrl: './new-listing-page.component.html',
-  styleUrls: ['./new-listing-page.component.css']
+  styleUrls: ['./new-listing-page.component.css'],
 })
 export class NewListingPageComponent implements OnInit {
+  name: string = '';
+  description: string = '';
+  price: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
   }
 
+  onSubmit(): void {
+    alert('Creating a new listing...');
+    this.router.navigateByUrl('/my-listings');
+  }
 }
