652cb6860941defd38c4e1d1f64823bbaae7d4fc
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-09-02 15:47:07 +0200

    Create initial state for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'buy-sell';
+  // title = 'buy-sell';
+  newMemberName = "";
+  members: string[] = [];
 }
