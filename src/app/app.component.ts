5816220e2bef56620c3024505ad4b6573426e9b3
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-09-02 16:00:45 +0200

    Save state of input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   // title = 'buy-sell';
-  newMemberName = "";
+  newMemberName = '';
   members: string[] = [];
+
+  onInput(value: string) {
+    this.newMemberName = value;
+  }
+
+  addMember() {
+    console.log('Clicked event');
+    if (this.newMemberName !== '') {
+      this.members.push(this.newMemberName);
+      console.log(this.members);
+      this.newMemberName = '';
+    }
+  }
 }
