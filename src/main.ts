ce81acea065f5a52d3154fb3871f0edec8c0f8ba
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-09-05 13:29:11 +0200

    Use pipe operator map in rxjs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -13,18 +14,83 @@ platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
 
+const users = {
+  data: [
+    {
+      status: 'active',
+      age: 14,
+    },
+    {
+      status: 'active',
+      age: 20,
+    },
+    {
+      status: 'inactive',
+      age: 43,
+    },
+    {
+      status: 'inactive',
+      age: 73,
+    },
+  ],
+};
+const users2 = {
+  data: [
+    {
+      status: 'active',
+      age: 14,
+    },
+    {
+      status: 'active',
+      age: 24,
+    },
+    {
+      status: 'inactive',
+      age: 43,
+    },
+    {
+      status: 'inactive',
+      age: 73,
+    },
+  ],
+};
+
 const observable = new Observable((subscriber) => {
-  subscriber.next(10);
-  subscriber.next(11);
-  subscriber.next(12);
-});
+  subscriber.next(users2);
+}).pipe(
+  map((value: any) => {
+    console.log('1) data from observable: ', value);
+
+    return value.data;
+  }),
+  map((value: any) => {
+    console.log('2) data from operator: ', value);
+
+    return value.filter((user: any) => user.status === 'active');
+  }),
+  map((value: any) => {
+    console.log('3) data from operator: ', value);
+    return (
+      value.reduce((acc: any, currentValue: any) => {
+        return acc + currentValue.age;
+      }, 0) / value.length
+    );
+  }),
+  map((value: any) => {
+    console.log('4) data from operator: ', value);
+      if (value < 18) {
+        throw new Error("Average age is too young");
+      }
+    return value;
+  })
+);
 
 const observer = {
   next: (value: any) => {
     console.log('Observer got a value of: ', value);
   },
   error: (err: any) => {
-    console.log('Observer got an error of: ', err);
+    console.error('Observer got an error of: ', err);
   },
   complete: () => {
     console.log('Observer got a complete notification');
